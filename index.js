7be3316859a7310e1720b27c26a68edf956e14dd
MERCYGATUNE <ariri691@gmail.com>
2024-03-24 12:39:08 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()*4)+3;
+console.log(random);
+
+
+ let num3 = 13;
+ let num4= 9;
+ let mod = num3 % num4 ;
+
+
+ 
+let max = Math.max(5,10,15,20);
+console.log(max);
